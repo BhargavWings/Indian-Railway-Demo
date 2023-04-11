bfbac47aa806baf256d4ed4b65b5c9f6f271dc1e
Bhargav Fofandi <bhargav.f@wingsts.com>
2023-04-11 12:37:02 +0530

    video rendered with more clearity

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1715523.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 127090212, 128667437, 127614126, 127089832, 127089840, 127089841, 127090922, 127833817, 127606626, 127611218, 128059816, 128120013, 128394931, 128394933 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/127054630/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/127054631/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/127054629/1/ammo.js', 'preload' : true},
+];
